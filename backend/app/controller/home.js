13e8c196ffb832ac8da7c162b4cc6c7539d9f192
zhuxy <ghostcode521@gmail.com>
2022-07-18 17:28:48 +0800

    fix drawer

--- backend/app/controller/home.js
+++ backend/app/controller/home.js
@@ -27,6 +27,65 @@ class HomeController extends Controller {
       title
     };
   }
+
+  async addUser() {
+    const { ctx } = this
+    const { name } = ctx.request.body
+    
+    try {
+      const result = await ctx.service.home.addUser(name)
+      ctx.body = {
+        code: 200,
+        msg: '添加成功',
+        data:null
+      }
+    } catch (error) {
+      ctx.body = {
+        code: 500,
+        msg: '添加失败',
+        data:null
+      }
+    }
+  }
+
+  async editUser() {
+    const { ctx } = this
+    const { id, name } = ctx.request.body
+    
+    try {
+      const result = await ctx.service.home.editUser(id, name)
+      ctx.body = {
+        code: 200,
+        msg: '编辑成功',
+        data:null
+      }
+    } catch (error) {
+      ctx.body = {
+        code: 500,
+        msg: '编辑失败',
+        data:null
+      }
+    }
+  }
+
+  async deleteUser() {
+    const { ctx } = this
+    const { id } = ctx.request.body
+    try {
+      const result = await ctx.service.home.deleteUser(id)
+      ctx.body = {
+        code: 200,
+        msg: '删除成功',
+        data:null
+      }
+    } catch (error) {
+      ctx.body = {
+        code: 500,
+        msg: '删除失败',
+        data:null
+      }
+    }
+  }
 }
 
 module.exports = HomeController;
--- backend/app/service/home.js
+++ backend/app/service/home.js
@@ -15,6 +15,44 @@ class HomeService extends Service{
       return null
     }
   }
+
+  async addUser(name) {
+    const { ctx, app } = this
+
+    try {
+      const result = await app.mysql.insert('list',{name})
+      return result
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+  }
+
+  async editUser(id, name) {
+    const { ctx, app } = this
+    try {
+      let result = await app.mysql.update('list', { name }, {
+        where: {
+          id
+        }
+      })
+      return result
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+  }
+
+  async deleteUser(id) {
+    const { ctx, app } = this
+    try {
+      let result = await app.mysql.delete('list',{id})
+      return result
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+  }
 }
 
 module.exports = HomeService
